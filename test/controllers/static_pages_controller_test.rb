ac1647cc4e72e54952ef0480bc636a025b59337e
Jafer Daltro <jaferjr@gmail.com>
2021-03-31 15:19:01 -0300

    Implement basic login

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,6 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get root_url
-    assert_response :success
-    assert_select "title", "Home | AMT - OPERACIONAL"
-  end
 
   test "should get about" do
     get about_url
