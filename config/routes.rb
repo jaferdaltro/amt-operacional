261eb149986df1e144c474d1f98e8b96fc061969
Jafer Daltro <jaferjr@gmail.com>
2021-03-31 10:27:15 -0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/about'
+  root  'static_pages#home'
+  get  '/about',  to: 'static_pages#about'
+  resources :users
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,13 +2,13 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select "title", "Home | AMT - OPERACIONAL"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
     assert_select "title", "Sobre | AMT - OPERACIONAL"
   end
