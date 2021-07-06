2e8bc1ba94a2923f0e09886aa0111bb594844e02
Jafer Daltro <jaferjr@gmail.com>
2021-07-06 07:04:22 -0300

    email in index users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
   has_many :articles
   validates :name, presence: true, length: {maximum: 50}
   validates :username, presence: true, length: {maximum: 30}, uniqueness: {case_sensitive: false}
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :email, presence: false, length: {maximum: 50}, format: { with: VALID_EMAIL_REGEX }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: false, length: {maximum: 50}, format: { with: VALID_EMAIL_REGEX }
       
   has_secure_password
   # validates :password, presence: true, length: { minimum: 6 }
